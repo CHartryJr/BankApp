bd4f5ccb0a99450f0539acb50b6a3d29af46f414
Carl Hartry Jr <carlhartry+github@gmail.com>
2023-11-03 13:31:17 -0400

    update bank server for readablity

--- src/BankServer.java
+++ src/BankServer.java
@@ -1,138 +1,161 @@
 
 import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Scanner;
 import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * This program wil be used as a communication hub and will be the engine for query based operations
  */
-public class BankServer extends Thread {
-
-  private static AtomicInteger num = new AtomicInteger(0); // a count for the number of active threads change when done to boolean
+public class BankServer
+{
   private static boolean running = true; //will be uses to shut down server.
-  private ServerSocket serverSocket;
-  private Socket clientSocket;
-  private String threadName, query;
-  private int identifier;
-
-  BankServer(int i) {
-    try {
-      this.serverSocket = new ServerSocket(i);
-      identifier = 0;
-    } catch (IOException e) {
+  private static AtomicInteger index = new AtomicInteger(1);//used to keep a index on all threads
+  public static void main(String[] args) 
+  {
+    try
+    {
+      ServerSocket serverSocket = new ServerSocket(5000);
+      System.out.println("Server has started");
+      while (running) 
+      {
+        System.out.println("listening for connection");
+        Socket clientSocketSession = serverSocket.accept();// will listen for client connection halts operation on main thread will stop until accepted
+        new BankServer().newCommunication(clientSocketSession, index.incrementAndGet());
+        System.out.println("A new connection has been made"); //debug only.
+      }
+      serverSocket.close();
+    }catch(Exception e)
+    {
       e.printStackTrace();
     }
   }
-
-  private BankServer(Socket clientSocket, int identifier) {
+  /**
+   * This method will be used to listen can create new communications for server.
+   * @param s
+   * @param identifier
+   */
+  private void newCommunication(Socket s, int identifier)
+  {
+   new Communication(s,identifier).start();
+  }
+/**
+ * nested class that will host all communications. The communication data will be sql statements. 
+ */
+private class Communication extends Thread
+{
+  private Socket clientSocket;
+  private String threadName, Buffer;
+  private int identifier;
+  private Communication(Socket clientSocket, int identifier)
+  {
     this.clientSocket = clientSocket;
     this.identifier = identifier;
   }
+  public void run()// action done during thread
+  {
+    try 
+    {
+      //since running a while loop will hold up client actions we will on run in and out messages
+      threadName = String.format("%d from %s ", identifier, this.getName());
+      writeTransaction("Key"); // can send encryption key
+      Buffer = readTransaction();
+      //handshake
 
-  public void run() {
-    if (identifier == 0) { //client connection thread
-      try {
-        while (running) {
-          Socket sockIn = serverSocket.accept();
-          BankServer connection = new BankServer(sockIn, num.incrementAndGet());
-          System.out.println("A new connection has been made"); //debug only.
-          connection.start();
-        }
-        serverSocket.close();
-      } catch (Exception e) {
-        try {
-          serverSocket.close();
-          System.exit(0);
-        } catch (IOException e1) {
-          e1.printStackTrace();
-        }
-      }
-    } else { // messageing thread
-      try {
-        threadName = String.format("%d from %s ", identifier, this.getName());
-        writeTransaction("Key"); // can send encryption key
-        query = (String) readTransaction();
-        this.wait(3000);
-        query = getResults(query);
-        this.wait(1000);
-        writeTransaction(query);
-        System.out.println(String.format("Competed Query From %s", threadName));
+
+      this.wait(3000);
+      Buffer = getResults(Buffer);//parse query
+      this.wait(1000);
+      writeTransaction(Buffer);//it out
+      System.out.println(String.format("Competed Query From %s", threadName));
+      index.getAndDecrement();
+      clientSocket.close();
+    } catch (Exception e) 
+    {
+      try 
+      {
         clientSocket.close();
-      } catch (Exception e) {
-        try {
-          clientSocket.close();
-          System.exit(1);
-        } catch (IOException e1) {
-          System.out.println("Socket did not Close");
-          System.exit(1);
-        }
+        System.exit(1);
+      } catch (IOException e1) 
+      {
+        System.out.println("Socket did not Close");
+        System.exit(1);
       }
     }
   }
-   // method to get sql statement from db
-  private synchronized String getResults(String query) {
-    Connection con = getConnection();
-    java.sql.Statement st;
-    ResultSet rs;
-    String result = "";
-    try {
-      st = con.createStatement();
-      rs = st.executeQuery(query);
-      while (rs.next()) {
-        result += rs.getString("") + "," + rs.getString(result);
+  /**
+   * method  read data recieved from user.
+   * @return void
+   */
+  private String readTransaction() 
+  {
+    String data = null;
+    try 
+    {
+      Scanner input = new Scanner(clientSocket.getInputStream());
+      while(input.hasNextLine())
+      {
+        data += input.nextLine();
       }
-    } catch (SQLException e) {
+      input.close();
+      return input.nextLine();
+    } catch (Exception e) 
+    {
       e.printStackTrace();
     }
-    return result;
-  }
-// method to read in messages
-  private Object readTransaction() {
-    try {
-      ObjectInputStream input = new ObjectInputStream(
-        clientSocket.getInputStream()
-      );
-      return input.readObject();
-    } catch (Exception e) {
-      e.printStackTrace();
-    }
-    return null;
+    return data;
   }
 // method to write out messages
-  private void writeTransaction(Object o) {
-    try {
-      ObjectOutputStream output = new ObjectOutputStream(
-        clientSocket.getOutputStream()
-      );
-      output.writeObject(o);
-    } catch (Exception e) {
+  private void writeTransaction(String data) 
+  {
+    try 
+    {
+      PrintWriter output = new PrintWriter(clientSocket.getOutputStream());
+      output.write(data);
+    } catch (Exception e)
+    {
       e.printStackTrace();
     }
   }
   // returns a db connection
-  private Connection getConnection() {
-    Connection con = null;
-    try {
-      con =
-        DriverManager.getConnection(
-          "jdbc:sqlite:" + getClass().getResource("../assets/Data/Bank.db")
-        );
-    } catch (SQLException e) {
-      e.printStackTrace();
+    private Connection getConnection() 
+    {
+      Connection con = null;
+      try 
+      {
+        con =DriverManager.getConnection("jdbc:sqlite:" + getClass().getResource("../assets/Data/Bank.db"));
+      } catch (SQLException e) 
+      {
+        e.printStackTrace();
+      }
+      return con;
     }
-    return con;
-  }
 
-  public static void main(String[] args) {
-    System.out.println("Server has started");
-    BankServer BS = new BankServer(5000);
-    BS.run();
+    private synchronized String getResults(String query)
+    {
+      Connection con = getConnection();
+      java.sql.Statement st;
+      ResultSet rs;
+      String result = "";
+      try 
+      {
+        st = con.createStatement();
+        rs = st.executeQuery(query);
+        while (rs.next()) 
+        {
+          result += rs.getString("") + "," + rs.getString(result);
+        }
+      } catch (SQLException e)
+      {
+        e.printStackTrace();
+      }
+      return result;
+    }
   }
 }
