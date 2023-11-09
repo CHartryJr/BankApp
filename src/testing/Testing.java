00de484e1100113298aae3d5ade2a2272e7e53d8
Carl Hartry Jr <carlhartry+github@gmail.com>
2023-11-08 19:35:42 -0500

    make file update

--- src/testing/Testing.java
+++ src/testing/Testing.java
@@ -1,22 +1,17 @@
+import BankServer;
+
 package testing;
 
-
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
-
-import BankServer;
-import controllers.TellerEventController;
 public class Testing 
 {
    
 
-    @BeforeAll
+   @Test
     public static void startServer()
     {
        BankServer.main(null);
     }
-
-    @Test 
+    
     public void TellerLogin()
     {
         
