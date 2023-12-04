d728b2d67a8336ce435ad7f1c47921cae14cb5b4
Brady Blackstone <147117036+Brady-Blackstone@users.noreply.github.com>
2023-12-04 13:57:33 -0500

    update client GUI

--- src/controllers/client/clientLoginController.java
+++ src/controllers/client/clientLoginController.java
@@ -1,6 +1,44 @@
 package controllers.client;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleButton;
 
-public class clientLoginController 
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import controllers.GUIOperation;
+
+public class clientLoginController extends GUIOperation implements Initializable 
 {
+    @FXML
+    private TextField tfUsername;
+    @FXML
+    private PasswordField tfPassword;
+    @FXML
+    private Button btnLogin;
+    @FXML
+    private ToggleButton btnCAcct;
     
+    void login(ActionEvent event) 
+    {
+
+    }
+
+  
+    void switchToCreate(ActionEvent event)
+    {
+
+    }
+
+    // main 
+    @Override
+    public void initialize(URL location, ResourceBundle resources)
+    {
+        btnLogin.setOnAction(this::login);
+        btnCAcct.setOnAction(this::switchToCreate);
+    }
 }
